840cc0f5f3bddaae4254237d6007cd9bcd87ee4b
synnectify01-sketch <synnectify.01@gmail.com>
2025-11-03 18:46:18 +0530

    Ensure mobile navbar icon is permanently visible with gradient colors by adding comprehensive CSS overrides for all possible states and devices

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0fvmjuk6118"
+    "revision": "0.6g678t05o7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
