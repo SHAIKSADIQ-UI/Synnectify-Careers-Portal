115b1bb1d46a7869e3d0c0b7fcad71dbe0fd4f4c
synnectify01-sketch <synnectify.01@gmail.com>
2025-11-03 19:01:05 +0530

    Completely remove hover effects from mobile menu button to ensure permanent visibility

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6g678t05o7g"
+    "revision": "0.kif1n7b8teg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
